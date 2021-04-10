72e00df29f1c4fece51c9191a1f380a12aedb353
ArhumRaza <arhumraza95@gmail.com>
2021-04-09 20:35:57 -0400

    Implemented Hashmap datastore
    Will need to improve id generation key.

--- src/main/java/com/rogers/codingassignment/service/impl/HashMapPersonServiceImpl.java
+++ src/main/java/com/rogers/codingassignment/service/impl/HashMapPersonServiceImpl.java
@@ -1,37 +1,41 @@
 package com.rogers.codingassignment.service.impl;
 
-import com.rogers.codingassignment.model.Gender;
 import com.rogers.codingassignment.model.Person;
 import com.rogers.codingassignment.service.PersonService;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.*;
 
 @Service
 public class HashMapPersonServiceImpl implements PersonService {
+    private Map<Integer,Person> personMap = new HashMap<>();
+    private static int ID = 0;
 
     @Override
     public List<Person> findAll() {
-       Person person = new Person((short) 18, "John", LocalDate.of(2002, 9,12), Gender.MALE, List.of());
-       List<Person> personList = new ArrayList<>();
-       personList.add(person);
-        return personList;
+       return new ArrayList<>(personMap
+               .values());
     }
 
     @Override
     public Person findById(int id) {
-        Person person = new Person((short) 18, "Bob", LocalDate.of(2002, 9,12), Gender.MALE, List.of());
-        return person;
+        return personMap.get(id);
     }
 
     @Override
     public int save(Person person) {
-        return 0;
+        ID++;
+        personMap.put(ID, person);
+        return 201;
     }
 
     @Override
     public void update(int id, Person person) {
-
+        personMap.replace(id, person);
     }
+
+//    @Override
+//    public void delete(int id) {
+//        personMap.remove(id);
+//        ID--;
+//    }
 }
