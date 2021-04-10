3bc083c47b8876c93cb55e7b1da9078de05dfe74
ArhumRaza <arhumraza95@gmail.com>
2021-04-09 21:16:32 -0400

    Created a PersonNotFoundException

---
+++ src/main/java/com/rogers/codingassignment/model/PersonNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rogers.codingassignment.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Invalid ID")
+public class PersonNotFoundException extends RuntimeException{
+
+    public PersonNotFoundException(int id) {
+        super(new StringBuilder().append("Person with id: ").append(id).append(" does not exist").toString());
+    }
+}
