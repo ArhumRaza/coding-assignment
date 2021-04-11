cb3fbc4c2982a7c87ed334097550bfdcc094c394
ArhumRaza <arhumraza95@gmail.com>
2021-04-11 12:53:43 -0400

    completed person model unit test

---
+++ src/test/java/com/rogers/codingassignment/model/PersonTest.java
@@ -0,0 +1,35 @@
+package com.rogers.codingassignment.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonTest {
+
+    private static final List<Person> personList = new ArrayList<>();
+
+    private static final Person youngestPerson = new Person((short) 15, "Youngest", LocalDate.now(), Gender.FEMALE, List.of());
+    private static final Person middlePerson = new Person((short) 17, "Middle", LocalDate.now(), Gender.MALE, List.of());
+    private static final Person oldestPerson = new Person((short) 20, "Oldest", LocalDate.now(), Gender.MALE, List.of());
+
+    @BeforeAll
+    static void setUp(){
+        personList.addAll(List.of(middlePerson, oldestPerson, youngestPerson));
+    }
+
+    @Test
+    @DisplayName("teenager method - between 13-18 years old and sorted by age youngest to oldest")
+    void teenagers(){
+
+        List<String> filteredPersonList = Person.teenagers(personList);
+
+        Assertions.assertEquals(filteredPersonList.size(), 2); // "Oldest should be filtered out
+        Assertions.assertEquals(filteredPersonList.get(0), youngestPerson.getName()); // should be "Youngest"
+        Assertions.assertEquals(filteredPersonList.get(1), middlePerson.getName()); // should be "Middle"
+    }
+}
