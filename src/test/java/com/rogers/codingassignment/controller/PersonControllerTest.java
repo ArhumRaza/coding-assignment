317c32c095d0ef2538ce65d4db78df4b0524ce67
ArhumRaza <arhumraza95@gmail.com>
2021-04-10 23:45:56 -0400

    controller test completed (unit test)

---
+++ src/test/java/com/rogers/codingassignment/controller/PersonControllerTest.java
@@ -0,0 +1,127 @@
+package com.rogers.codingassignment.controller;
+
+import com.rogers.codingassignment.model.Gender;
+import com.rogers.codingassignment.model.Person;
+import com.rogers.codingassignment.model.PersonNotFoundException;
+import com.rogers.codingassignment.service.PersonService;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import java.time.LocalDate;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = PersonController.class)
+public class PersonControllerTest {
+
+    private final Person testPerson  = new Person((short) 26, "John", LocalDate.of(1994, 9, 2), Gender.MALE, List.of("Oakville"));
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PersonService personService;
+
+    @Test
+    @DisplayName("retrieve all people - success")
+    void retrieveAllPeople() throws Exception {
+        BDDMockito.given(personService.findAll()).willReturn(List.of(testPerson));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/person").accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].age").value(Matchers.is((int) testPerson.getAge())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].name").value(Matchers.is(testPerson.getName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].dob").value(Matchers.is(testPerson.getDob().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].gender").value(Matchers.is(testPerson.getGender().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].address").value(Matchers.is(testPerson.getAddress())));
+    }
+
+    @Test
+    @DisplayName("retrieve person - success")
+    void retrievePerson() throws Exception {
+        int id = 5;
+
+        BDDMockito.given(personService.findById(id)).willReturn(testPerson);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/person/" + id))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(Matchers.is((int) testPerson.getAge())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(Matchers.is(testPerson.getName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dob").value(Matchers.is(testPerson.getDob().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value(Matchers.is(testPerson.getGender().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address").value(Matchers.is(testPerson.getAddress())));
+    }
+
+    @Test
+    @DisplayName("retrieve person - id not found -> 404")
+    void retrievePersonIdNotFound() throws Exception {
+        int id = 5;
+
+        BDDMockito.given(personService.findById(id)).willThrow(new PersonNotFoundException(id));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/person/" + id).accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("create person - success")
+    void createPerson() throws Exception {
+        int id = 99;
+
+        BDDMockito.given(personService.save(BDDMockito.any())).willReturn(id);
+
+        String requestBody = new JSONObject()
+                .put("age", 4)
+                .put("name", "John")
+                .put("dob", LocalDate.of(1996, 02, 01).toString())
+                .put("gender", Gender.OTHER)
+                .toString();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/person").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.header().string("Location", "http://localhost:8080/person/" + id));
+
+    }
+
+    @Test
+    @DisplayName("update person - person")
+    void updatePerson() throws Exception {
+        int id = 1;
+
+        BDDMockito.doNothing().when(personService).update(BDDMockito.eq(id), BDDMockito.isA(Person.class));
+
+        String requestBody = new JSONObject()
+                .put("age", 20)
+                .put("name", "Doe")
+                .put("dob", LocalDate.of(1996, 02, 01).toString())
+                .put("gender", Gender.OTHER)
+                .toString();
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/person/" + id)
+                .contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+    }
+
+
+}
