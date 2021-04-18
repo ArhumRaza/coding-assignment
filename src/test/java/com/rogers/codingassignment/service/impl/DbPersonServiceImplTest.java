831a293eb4f310c85262406982dd4a3dbc9c1aec
ArhumRaza <arhumraza95@gmail.com>
2021-04-18 16:19:14 -0400

    finished implementation

--- src/test/java/com/rogers/codingassignment/service/impl/DbPersonServiceImplTest.java
+++ src/test/java/com/rogers/codingassignment/service/impl/DbPersonServiceImplTest.java
@@ -3,6 +3,7 @@ package com.rogers.codingassignment.service.impl;
 import com.rogers.codingassignment.model.Gender;
 import com.rogers.codingassignment.model.Person;
 import com.rogers.codingassignment.model.PersonNotFoundException;
+import com.rogers.codingassignment.repository.PersonEntity;
 import com.rogers.codingassignment.repository.PersonRepository;
 import com.rogers.codingassignment.service.PersonService;
 import org.junit.jupiter.api.Assertions;
@@ -27,9 +28,8 @@ class DbPersonServiceImplTest {
     @InjectMocks
     PersonService personService = new DbPersonServiceImpl();
 
-    private static final Person person1 = new Person((short) 20, "John", LocalDate.of(2001, 02, 04), Gender.MALE, List.of("Oakville"));
-    private static final Person person2 = new Person((short) 25, "Doe", LocalDate.of(1996, 02, 01), Gender.OTHER, List.of("Toronto"));
-    private static final Person updatedPerson = new Person((short) 25, "New Guy", LocalDate.of(1996, 02, 01), Gender.OTHER, List.of("Toronto"));
+    private static final PersonEntity personEntity1 = new PersonEntity((short) 20, "John", LocalDate.of(2001, 02, 04), Gender.MALE, List.of("Oakville"));
+    private static final PersonEntity personEntity2 = new PersonEntity((short) 25, "Doe", LocalDate.of(1996, 02, 01), Gender.OTHER, List.of("Toronto"));
 
     private static final int id = 1;
     private static final int invalidId = 99;
@@ -38,52 +38,79 @@ class DbPersonServiceImplTest {
     @Test
     @DisplayName("find all person")
     public void findAll() {
-        BDDMockito.given(personRepository.findAll()).willReturn(List.of(person1, person2));
-        Assertions.assertEquals(personService.findAll().size(), 2);
+        BDDMockito.given(personRepository.findAll()).willReturn(List.of(personEntity1, personEntity2));
+
+        List<Person> personList = personService.findAll();
+
+        Assertions.assertEquals(personList.size(), 2);
+
+        Assertions.assertEquals(personList.get(0).getName(), personEntity1.getName());
+
+        Assertions.assertEquals(personList.get(1).getName(), personEntity2.getName());
     }
 
     @Test
     @DisplayName("find person by id")
     public void findById() {
-        BDDMockito.given(personRepository.findById(id)).willReturn(Optional.of(person1));
-        Assertions.assertEquals(personService.findById(id), person1);
+        BDDMockito.given(personRepository.findById(id)).willReturn(Optional.of(personEntity1));
+
+        Person testPerson = personService.findById(id);
+
+        Assertions.assertEquals(testPerson.getAge(), personEntity1.getAge());
+        Assertions.assertEquals(testPerson.getName(), personEntity1.getName());
+        Assertions.assertEquals(testPerson.getDob(), personEntity1.getDob());
+        Assertions.assertEquals(testPerson.getGender(), personEntity1.getGender());
+        Assertions.assertEquals(testPerson.getAddress(), personEntity1.getAddress());
     }
 
     @Test
     @DisplayName("find person by id - 404 not found due to invalid id")
     public void findByIdInvalidId() {
         BDDMockito.given(personRepository.findById(invalidId)).willReturn(Optional.empty());
+
         Assertions.assertThrows(PersonNotFoundException.class, () -> personService.findById(invalidId));
     }
 
     @Test
     @DisplayName("create new person")
     public void save() {
-        person2.setId(id);
-        BDDMockito.given(personRepository.save(person2)).willReturn(person2);
-        Assertions.assertEquals(personService.save(person2), person2.getId());
+        Person person = new Person((short) 25, "Doe", LocalDate.of(1996, 02, 01), Gender.OTHER, List.of("Toronto"));
+
+        personEntity2.setId(id);
+
+        BDDMockito.given(personRepository.save(BDDMockito.isA(PersonEntity.class))).willReturn(personEntity2);
+
+        Assertions.assertEquals(personService.save(person), personEntity2.getId());
     }
 
     @Test
     @DisplayName("update person by id")
     public void update() {
-        BDDMockito.given(personRepository.findById(id)).willReturn(Optional.of(person1));
-        BDDMockito.given(personRepository.save(updatedPerson)).willReturn(updatedPerson);
+        Person updatedPerson = new Person((short) 25, "New Guy", LocalDate.of(1996, 02, 01), Gender.OTHER, List.of("Toronto"));
+        PersonEntity updatedPersonEntity = new PersonEntity((short) 25, "New Guy", LocalDate.of(1996, 02, 01), Gender.OTHER, List.of("Toronto"));
+
+        BDDMockito.given(personRepository.findById(id)).willReturn(Optional.of(updatedPersonEntity));
+        BDDMockito.given(personRepository.save(BDDMockito.isA(PersonEntity.class))).willReturn(updatedPersonEntity);
+
         personService.update(id, updatedPerson);
     }
 
     @Test
     @DisplayName("update person by id - 404 not found due to invalid id")
     public void updateInvalidId() {
+        Person updatedPerson = new Person((short) 25, "New Guy", LocalDate.of(1996, 02, 01), Gender.OTHER, List.of("Toronto"));
+
         BDDMockito.given(personRepository.findById(invalidId)).willReturn(Optional.empty());
+
         Assertions.assertThrows(PersonNotFoundException.class, () -> personService.update(invalidId, updatedPerson));
     }
 
     @Test
     @DisplayName("delete person by id")
     public void delete() {
-        BDDMockito.given(personRepository.findById(id)).willReturn(Optional.of(person1));
+        BDDMockito.given(personRepository.findById(id)).willReturn(Optional.of(personEntity1));
         BDDMockito.doNothing().when(personRepository).deleteById(id);
+
         personService.delete(id);
     }
 
